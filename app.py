750fa522f19974c79c6d209a7d707b1a1b7189f5
Dmitry Polyakov <liltechdude@gmail.com>
2020-12-12 19:46:05 +0500

    fix name

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def index():
 def authorizify(id: int) -> str:
     authors = {
         -144705371: ["https://vk.com/ryukishi07", "ORIGINAL"],
-        -81413361: ["https://vk.com/id70251320", "Алексей Кушелов"],
+        -81413361: ["https://vk.com/id70251320", "Алексей Кулешов"],
         -89327841: ["https://vk.com/xx_gensokyo_xx", "EXTREME RUSSIAN FANDOM FOR TOUHOU"],
         -192241227:["https://vk.com/clocktowerzines", "Clock Tower Zines | TYPE-MOON zine | WTCZINE"]
     }
